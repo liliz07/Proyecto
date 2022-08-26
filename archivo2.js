c3ac4b72f8f2cac9895849a94243cc3dccfe28ef
lili_ana711@hotmail.com <lsantos@saludbc.gob.mx>
2022-08-25 18:20:25 -0700

    Modifique el archivo js

--- archivo2.js
+++ archivo2.js
@@ -1,7 +1,7 @@
 function bienvenida(){
     
     console.log("Bienvenido desde JS");
-    document.getElementById("saludo").innerHTML="Hola HTML";
+    document.getElementById("saludo").innerHTML="Esta es la ultima version";
     
 }
 bienvenida(); //invocando la funcion
